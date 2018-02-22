3ca35cb22697a8e5791fe17a257089a4f85c213f
Garrett Sickles <garrettsickles@gmail.com>
2018-02-22 00:07:27 -0700

    Fixing up Example 1 to reflect the readme

--- example/ExampleOne.unit_test.cpp
+++ example/ExampleOne.unit_test.cpp
@@ -1,24 +1,39 @@
 #include <PopQuiz.hpp>
 
 void PopQuiz::Setup() {
-    
-	OutputJSON("ExampleOne.unit_test.json");
 
+    OutputJSON("Example.unit_test.json");
+
+    // Add a unit test in suite 'Example #1' named 'Do Nothing'
     AddTest("Example #1", "Do Nothing", [](void) {
-
+        // Pass by default
     });
 
+    // Add a unit test in suite 'Example #1' named 'Assert True'
     AddTest("Example #1", "Assert True", [](void) {
+        // Assert 1 == 1 with the message 'Equal, Yay!'
         AssertEqual(1, 1, "Equal, Yay!");
     });
-
-    AddTest("Example #1", "Throw Exception", [](void) {
-        AssertEqual(1, 2, "Not equal, duh...");
-    }, false);
-
+    
+    // Add a unit test in suite 'Example #1' named 'Expect Exception'
     AddTest("Example #1", "Expect Exception", [](void) {
+        // Assert that we throw a 'std::runtime_error'
         AssertThrow<std::runtime_error>([](){
-            throw std::runtime_error("Hello!");
-        });
-    });
+            throw std::runtime_error("This is an error");
+        }); 
+    }, false);
+    
+    // Add a unit test in suite 'Example #1' named 'Throw Exception'
+    AddTest("Example #1", "Throw Exception", [](void) {
+        // Assert 1 == 2 with the message 'Not equal, duh...'
+        AssertEqual(1, 2, "Not equal, duh..."); 
+    },
+        // Passing 'false' to 'AddTest' after the test lambda
+        // will run the test but ignore the result so all other
+        // tests will run. This allows you to add a test knowing
+        // it is broken but still include it in your test suite.
+        // This test will not affect the return code of the unit
+        // test suite if set to 'false'.
+        false
+    );
 }
